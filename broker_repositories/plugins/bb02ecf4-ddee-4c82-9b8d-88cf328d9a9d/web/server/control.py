53dde6f98f8ea2767b2737fc2f1f02be34298c68
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-01-18 23:10:48 +0100

    Take broker functionality and put in broker, move rest of control functionality to plug in

---
+++ broker_repositories/plugins/bb02ecf4-ddee-4c82-9b8d-88cf328d9a9d/web/server/control.py
@@ -0,0 +1,133 @@
+"""
+This module exposes the Optimal BPM Admin API as a web service through a CherryPy module
+"""
+
+import copy
+import cherrypy
+
+from mbe.cherrypy import aop_check_session
+from mbe.constants import object_id_right_admin_everything
+from mbe.groups import has_right
+from .lib.control import Control
+from optimalbpm.common.messaging.factory import start_process_message
+from optimalbpm.common.messaging.utils import get_environment_data
+from optimalbpm.broker.globals import states
+
+__author__ = 'Nicklas Borjesson'
+
+
+class CherryPyControl(object):
+    """
+    The CherryPyControl class is a plugin that proves process and agent control functions.
+    All functionality that concerns running, stopping and restarting BPM functionality is gathered here.
+    Each function:
+    * checks credentials, rights and validates using AOP decorators
+    * transforms the request and forwards to the counterparts in the Administration class,
+    """
+
+    states = None
+
+    def __init__(self, _broker_object):
+        self._administration = Control(_broker_object.database_access, _broker_object.node, _broker_object.monitor.queue, _broker_object.stop_broker,
+                                              _broker_object.address, _broker_object.process_id)
+
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def save_process_definition(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        return self._administration.save_process_definition(cherrypy.request.json, kwargs["user"])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def load_process_definition(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        return self._administration.load_process_definition(cherrypy.request.json, kwargs["user"])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def remove_process_definition(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        return self._administration.remove_process_definition(cherrypy.request.json, kwargs["user"])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def get_process_definition_hash(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        return self._administration.get_process_definition_hash(cherrypy.request.json, kwargs["user"])
+
+
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def agent_control(self, **kwargs):
+        return self._administration.agent_control(cherrypy.request.json["address"],
+                                                  cherrypy.request.json["command"],
+                                                  cherrypy.request.json["reason"],
+                                                  kwargs["user"])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def start_process(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        if "source_process_id" in cherrypy.request.json:
+            _source_process_id = cherrypy.request.json["source_process_id"]
+        else:
+            _source_process_id = None
+
+        _start_process_message = start_process_message(
+            _user_id=kwargs["user"]["_id"],
+            _process_definition_id=cherrypy.request.json["process_definition_id"],
+            _destination=cherrypy.request.json["destination"],
+            _globals=cherrypy.request.json["globals"],
+            _reason=cherrypy.request.json["reason"],
+            _message_id=cherrypy.request.json["message_id"],
+            _source_process_id=_source_process_id
+        )
+
+        return self._administration.start_process(_start_process_message=_start_process_message, _user=kwargs["user"])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def get_broker_environment(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        print("Request for broker information")
+        return get_environment_data()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def get_processes(self, **kwargs):
+        print("Request for a list of processes")
+        return self._administration.get_processes(kwargs["user"])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def get_process_states(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        print("Request for a list of states")
+        return list(copy.copy(states))
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out(content_type='application/json')
+    @aop_check_session
+    def get_process_history(self, **kwargs):
+        has_right(object_id_right_admin_everything, kwargs["user"])
+        print("Request for history of a process")
+        _process_id = cherrypy.request.json["process_id"]
+        return self._administration.get_process_history(_process_id, kwargs["user"])
