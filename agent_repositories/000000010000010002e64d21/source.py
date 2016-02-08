9d85ad26f1b03847a1ad66d3603cd58c223a36d4
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-02-08 23:23:58 +0100

    Add second step and metadata files

--- agent_repositories/000000010000010002e64d21/source.py
+++ agent_repositories/000000010000010002e64d21/source.py
@@ -5,5 +5,3 @@ It is neither runnable or thought of as a example.
 
 """Get data from a data source"""
 dataset = qal.merge_datasets(get_data("000000000000000000001234"))
-"""Try and query the database"""
-qal.query(get_data("56b913fb5552e1ad238e1498"))
