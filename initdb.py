22ebadbd62d3da2c02064c7577fd7441fd9ead6e
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-04-17 12:15:45 +0200

    Add docstrings

--- initdb.py
+++ initdb.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
-from plugins.optimalbpm.testing.init_env import init_bpm_env
+
+"""
+This script initiates the Optimal BPM database with a base structure
+"""
+
+from optimalbpm.testing.init_env import init_bpm_env
 
 init_bpm_env()
--- initdb.sh
+++ initdb.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+
+# This script initiates the Optimal BPM database with a base structure
+
 python3 initdb.py
