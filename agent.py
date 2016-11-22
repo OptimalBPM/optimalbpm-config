cb6f9dffe53e642d02f9930b967ec2205e8e03ac
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-11-22 23:35:53 +0100

    Update configs

--- agent.py
+++ agent.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+
+"""
+    This script starts the Optimal BPM agent
+"""
+
+import os
+script_dir = os.path.dirname(__file__)
+
 from plugins.optimalbpm.agent.agent import start_agent
 
-start_agent()
+start_agent(_cfg_filename=os.path.join(script_dir, "config.json"))
--- broker.py
+++ broker.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+    This script initiates the Optimal Framework database with a base structure
+"""
+import os
+script_dir = os.path.dirname(__file__)
 from of.broker.broker import start_broker
 
-start_broker()
+start_broker(_cfg_filename=os.path.join(script_dir, "config.json"))
