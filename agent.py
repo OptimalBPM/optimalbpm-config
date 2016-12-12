0e06f0c5a451ea9f63a686fe47d08f395c924b3b
ubpmuser <nicklasb@gmail.com>
2016-12-12 10:38:13 +0100

    Put in __main__-section to fix windows multiprocessing
    Windows imports the entire main module, since it has no fork() (sort of), hence it has to not accidentally re-run the initialisation of the whole system.

--- agent.py
+++ agent.py
@@ -4,9 +4,9 @@
     This script starts the Optimal BPM agent
 """
 
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
+if __name__ == "__main__":
+    import os
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-from plugins.optimalbpm.agent.agent import start_agent
-
-start_agent(_cfg_filename=os.path.join(script_dir, "config.json"))
+    from plugins.optimalbpm.agent.agent import start_agent
+    start_agent(_cfg_filename=os.path.join(script_dir, "config.json"))
--- broker.py
+++ broker.py
@@ -2,8 +2,9 @@
 """
     This script initiates the Optimal Framework database with a base structure
 """
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
-from of.broker.broker import start_broker
+if __name__ == "__main__":
+    import os
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    from of.broker.broker import start_broker
 
-start_broker(_cfg_filename=os.path.join(script_dir, "config.json"))
+    start_broker(_cfg_filename=os.path.join(script_dir, "config.json"))
